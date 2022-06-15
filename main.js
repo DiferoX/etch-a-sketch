54b54d53538448214ceae610d06d5e0d4ff4d4b8
Diego <diferox@gmail.com>
2022-06-15 01:37:32 -0500

    add a red button and a black button to paint the board

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
+let board = document.querySelector('.board');
+
 function createDivs (rows, cols)
 {
-  let board = document.querySelector('.board');
   board.style.setProperty ('--grid-rows', rows);
   board.style.setProperty ('--grid-cols', cols);
-  //board.style.gridTemplateColumns = 'repeat (${x} , 1fr)';
-  //board.style.gridTemplateRows = 'repeat (${X}, 1fr)';
 
   for (let i= 0; i< (rows*cols); i++)
   {
       let divs = document.createElement('div');
-      //divs.classList.add('divs');
-      divs.setAttribute ('style', 'background: red');
-      board.insertAdjacentElement('beforeend', divs);
-      //board.style.gridTemplateColumns = 'repeat (${i}, 1fr)';
-      //board.style.gridTemplateRows = 'repeat (${i}, 1fr)';
+      divs.classList.add('divs');
 
-      //board.appendChild(divs);
+      divs.setAttribute ('style', 'background: white');
+      //board.insertAdjacentElement('beforeend', divs);
+
+      board.appendChild(divs);
   }
 }
 
 createDivs(16, 16);
+
+let divs = board.querySelectorAll('.divs');
+
+
+let btnBlack = document.getElementsByClassName('btnBlack');
+
+btnBlack[0].addEventListener('click', function() 
+{
+  divs.forEach(divs => divs.addEventListener ('mouseover', () => 
+  {
+    divs.style.backgroundColor = 'black';
+  }));
+});
+
+
+let btnRed = document.getElementsByClassName('btnRed');
+
+btnRed[0].addEventListener('click', function() 
+{
+  divs.forEach(divs => divs.addEventListener ('mouseover', () => 
+  {
+    divs.style.backgroundColor = 'red';
+  }));
+});
+
+
